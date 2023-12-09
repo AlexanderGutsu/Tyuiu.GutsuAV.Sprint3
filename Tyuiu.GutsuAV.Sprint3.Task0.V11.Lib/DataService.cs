0412c10a4f5afae49d8e6531bebe15e8f1843da0
Tyuiu.GutsuAV@outlook.com <gutsu05@mail.ru>
2023-12-09 19:02:31 +0500

    Task0

---
+++ Tyuiu.GutsuAV.Sprint3.Task0.V11.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.GutsuAV.Sprint3.Task0.V11.Lib
+{
+    public class DataService : ISprint3Task0V11
+    {
+        public double GetSumSeries(int value, int startValue, int stopValue)
+        {
+            double sumSeries = 0;
+            int i;
+            for (i = startValue; i <= stopValue; i++)
+            {
+                sumSeries += (1 / Math.Pow(value, i)) * (1 / Math.Pow(value, i));
+            }
+
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.GutsuAV.Sprint3.Task0.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GutsuAV.Sprint3.Task0.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GutsuAV.Sprint3.Task0.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("520dab35-d035-460b-a43c-bf0e7863ad5c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
